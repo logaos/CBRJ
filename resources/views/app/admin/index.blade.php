5a62ed0dcceb19fb645117d49ca96429d59dad4b
logaos <130524131+logaos@users.noreply.github.com>
2024-02-14 10:30:49 +0800

    Initial commit

---
+++ resources/views/app/admin/index.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('footer')
+    <div style="text-align: center">
+    Copyright &copy; 2024. <strong>CBRJ Admin</strong>. All rights reserved.
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ resources/views/homepage/contactus.blade.php
@@ -0,0 +1,9 @@
+@extends('homepage.template');
+
+@section('pageTitle')
+    Contacts
+@endsection
+
+@section('pageContent')
+    This is the content
+@endsection
